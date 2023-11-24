d528418ee5f75458d7f131a649018990d79bf1ec
Maarten A. Breddels <maartenbreddels@gmail.com>
2023-11-24 15:48:31 +0100

    fix: 100% height was not set because we used a custom style for the AppLayout

--- solarathon/pages/__init__.py
+++ solarathon/pages/__init__.py
@@ -33,4 +33,4 @@ def Page():
 @solara.component
 def Layout(children):
     # this is the default layout, but you can override it here, for instance some extra padding
-    return solara.AppLayout(children=children, style={"padding": "20px"})
+    return solara.AppLayout(children=children, style={"padding": "20px", "height": "100%"})
